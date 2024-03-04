5a50068287a4771111bcb815edb490af3bd13439
Luizenys <luizars@id.uff.br>
2024-03-04 19:07:56 -0300

    feat: form validation with zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,28 @@
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod'
+
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
+})
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema)
+    })
 
     const task = watch('task')
     const isSubmitDisabled = !task
 
     function handleCreateNewCycle(data: any) {
-
+        console.log(data)
     }
+
+    console.log(formState.errors)
 
     return (
         <HomeContainer>
