76e92d6424f961f9f94e91e2b5d8df19a3858d44
Luizenys <luizars@id.uff.br>
2024-03-05 12:51:16 -0300

    feat: starting the countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
 
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Informe a tarefa'),
@@ -21,7 +22,8 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 interface Cycle {
     id: string,
     task: string,
-    minutesAmount: number
+    minutesAmount: number,
+    startDate: Date
 }
 
 export function Home() {
@@ -37,11 +39,24 @@ export function Home() {
         }
     })
 
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    useEffect(() => {
+        if (activeCycle) {
+            setInterval(() => {
+                setAmountSecondsPassed(
+                    differenceInSeconds(new Date(), activeCycle.startDate)
+                )
+            }, 1000)
+        }
+    }, [activeCycle])
+
     function handleCreateNewCycle(data: NewCycleFormData) {
         const newCycle: Cycle = {
             id: String(new Date().getTime()),
             task: data.task,
-            minutesAmount: data.minutesAmount
+            minutesAmount: data.minutesAmount,
+            startDate: new Date()
         }
 
         setCycles((state) => [...state, newCycle])
@@ -54,8 +69,6 @@ export function Home() {
 
     const task = watch('task')
     const isSubmitDisabled = !task
-
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
     const amountSecondsRemaining = activeCycle ? totalSeconds - amountSecondsPassed : 0
